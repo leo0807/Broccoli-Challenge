cfc96deaeae4f78d50bbb9c6ffd89363f7a01ca9
leo0807 <zhangjunxu3@gmail.com>
2021-11-24 02:55:52 +0800

    update

--- src/utils/service.tsx
+++ src/utils/service.tsx
@@ -1,4 +1,4 @@
 import Axios from 'axios';
-const URL: string = "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth";
+const URL = "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth";
 export const postRegister = (data: any) => 
     Axios.post(URL, data);
